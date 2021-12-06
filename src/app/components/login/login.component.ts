72d372a0535966f116e675d01c8d93c58ff3695d
Ahmad Amjad <79445941+ahmad-hawklogix001@users.noreply.github.com>
2021-12-06 17:36:24 +0500

    UI text and alignment changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,21 +11,11 @@ export class LoginComponent implements OnInit {
   constructor(public loginservice:LoginService) { }
 
   ngOnInit(): void {
-    localStorage.setItem('name', 'Bob')
-    console.log(localStorage.getItem('name'))
-    document.cookie='name=Kyle; expires=' + new Date(2022, 1, 12 ).toUTCString();
+
   }
   rememberMe(){
     var email= document.getElementById('email');
   }
-// loginUser(){
-//   console.warn(this.loginForm.value)
-// }
-// get user(){
-// return this.loginForm.get('user')
-// }
-// get password(){
-// return this.loginForm.get('password')
-// }
+
 
 }
