3430bb6260d99e4b7114df9ed4904e65aca2aeb9
HusnainZafar563 <97527454+HusnainZafar563@users.noreply.github.com>
2022-01-17 15:41:12 +0500

    update

--- src/app/leave/leave.component.ts
+++ src/app/leave/leave.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { LeaveformComponent } from '../leaveform/leaveform.component';
 
 @Component({
   selector: 'app-leave',
@@ -10,28 +8,19 @@ import { LeaveformComponent } from '../leaveform/leaveform.component';
 export class LeaveComponent implements OnInit {
   ID!: any;
   Name!: string;
-  Designation!: string;
+  Designation!:string;
   Sick!: number;
   Casual!: number;
   Annual!:number;
   Total!:number;
-  Action!:any;
-  dialog!:any;
 
 
 
 
-  displayedColumns: string[] = ['ID','Name','Designation','Sick','Casual','Annual','Total','Actions'];
+  displayedColumns: string[] = ['ID','Name','Designation','Sick','Casual','Annual','Total'];
   constructor() { }
 
   ngOnInit(): void {
   }
-onCreate(){
-  const dialogconfig = new MatDialogConfig();
-  dialogconfig.disableClose = true;
-  dialogconfig.autoFocus=true;
-  dialogconfig.width='60%'
-  this.dialog.open(LeaveformComponent,dialogconfig)
 
-}
 }
