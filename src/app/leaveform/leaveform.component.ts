6a456b8e55d59eb234384517b73e26c837b28b4a
HusnainZafar563 <97527454+HusnainZafar563@users.noreply.github.com>
2022-01-17 12:16:59 +0500

    update

--- src/app/leaveform/leaveform.component.ts
+++ src/app/leaveform/leaveform.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+interface Leavetype {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-leaveform',
   templateUrl: './leaveform.component.html',
   styleUrls: ['./leaveform.component.scss']
 })
+// export class DateRangePickerOverviewExample {}
 export class LeaveformComponent implements OnInit {
-
+  LeaveType: Leavetype[] = [
+    { value: 'Sick', viewValue: 'Sick' },
+    { value: 'Annual', viewValue: 'Annual' },
+    { value: 'Casual', viewValue: 'Casual' },
+  ];
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
